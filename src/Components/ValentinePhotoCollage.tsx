de55e2bad2f192a177b5e8af421a95e9a90195bc
Jensen <omega90ej@gmail.com>
2025-02-10 22:30:01 -0800

    added

--- src/Components/ValentinePhotoCollage.tsx
+++ src/Components/ValentinePhotoCollage.tsx
@@ -8,8 +8,9 @@ export const ValentinePhotoCollage = () => {
             <div className="firework"></div>
             <div className="firework"></div>
             <div className='flex flex-col pt-20'>
-            <div className='flex flex-col justify-content scale-x-[-1]'>
+            <div className='flex flex-col justify-content'>
                 <h1 className='text-8xl text-center'>YAAYY</h1>
+                <h1 className='text-center text-4xl'>Here's a Capybara :D</h1>
                 <div className='flex justify-center items-center mt-10'>
                     <div className='slide-in-left w-[500px] h-[500px]'>
                         <img src={capy1} alt="Right CapyBara" className='scale-x-[-1] w-full h-full'/>
@@ -18,6 +19,10 @@ export const ValentinePhotoCollage = () => {
                         <img src={capy1} alt="Left CapyBara" className='w-full h-full'/>
                     </div>
                 </div>
+            </div>
+            <div className=''>
+                <h2>If you want to see the other things I did, click here</h2>
+                <button>Here</button>
             </div>
             </div>
         </div>
